3be4cd87675fa6ff236e248d5c4a54e4ac4d2c6f
harshali <harshalik@335.com>
2026-01-18 21:41:39 +0530

    Added C# Activity-1 code files

---
+++ ArithmeticOperations.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class ArithmeticOperations
+{
+    //static void Main()
+    //{
+    //    int a = 20, b = 5;
+
+    //    Console.WriteLine("Addition: " + (a + b));
+    //    Console.WriteLine("Subtraction: " + (a - b));
+    //    Console.WriteLine("Multiplication: " + (a * b));
+    //    Console.WriteLine("Division: " + (a / b));
+    //    Console.WriteLine("Modulus: " + (a % b));
+
+    //    Console.ReadLine();
+    //}
+}
---
+++ ArraySum.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class ArraySum
+{
+    static void Main()
+    {
+        int[] arr = { 1, 2, 3, 4, 5 };
+        int sum = 0;
+
+        foreach (int n in arr)
+        {
+            sum += n;
+        }
+
+        Console.WriteLine("Sum of array elements = " + sum);
+        Console.ReadLine();
+    }
+}
